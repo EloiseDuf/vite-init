16ed0a4ea88add353177049335b005b51b279048
ELOISE DUFAU <eloise.dufau@gmail.com>
2023-05-31 23:37:51 +0200

    exo 6 without bonus

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,46 @@ import PokemonCard from './components/PokemonCard'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew"
-  }
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+  
+  const previousClick = () => {
+    setPokemonIndex(pokemonIndex-1)
+  }
+
+  const nextClick= () => {
+    setPokemonIndex(pokemonIndex+1)
+  }
 
   return (
     <>
-      <PokemonCard name={pokemonList[1].name} imgSrc={pokemonList[1].imgSrc}  />
+      <PokemonCard name={pokemonList[pokemonIndex].name} imgSrc={pokemonList[pokemonIndex].imgSrc}  />
+    <button onClick={previousClick}>Previous</button>
+    <button onClick={nextClick}>Next</button>
     </>
   )
 }
