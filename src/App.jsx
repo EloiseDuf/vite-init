8e85b937b18ceab9d3e2aee08b6a97c381388db5
ELOISE DUFAU <eloise.dufau@gmail.com>
2023-05-31 12:52:58 +0200

    exercice 4

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew"
+  }
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PokemonCard/>
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc}  />
     </>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,42 +1,39 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew"
-  }
-];
 
-function PokemonCard(){
-    const pokemon=pokemonList[0];
-    const picture=pokemon.hasOwnProperty('imgSrc');
-   
-    return(
+// function PokemonCard(props){
+//     const pokemon={
+//       name:props.name,
+//       imgSrc:props.imgSrc
+//     }
+  
+//   const picture=pokemon.hasOwnProperty('imgSrc');
+//    console.log(props);
+    
+//    return(
 
-<figure>
-    <img style={{display: picture ? "inline" : "none"}} src={pokemon.imgSrc}
-         alt={pokemon.name}/>
-    <p style={{display: picture ? "none" : "inline"}}>???</p>
-    <figcaption>{pokemon.name}</figcaption>
-</figure>)
-}
-
-// autre solution proposé par Abdou:
-// function PokemonCard() {
-//   const pokemon = pokemonList[1];
-
-//   return (
-//     <figure>
-//       {pokemon.imgSrc != null ? (
-//         <img src={pokemon.imgSrc} alt={pokemon.name} />
-//       ) : (
-//         <p>???</p>
-//       )}
-//       <figcaption>{pokemon.name}</figcaption>
-//     </figure>
-//   );
+// <figure>
+//     <img style={{display: picture ? "inline" : "none"}} src={pokemon.imgSrc}
+//          alt={pokemon.name}/>
+//     {/* <p style={{display: picture ? "none" : "inline"}}>???</p> */}
+//     <figcaption>{pokemon.name}</figcaption>
+// </figure>)
 // }
+
+function PokemonCard(props) {
+  const pokemon={
+          name:props.name,
+          imgSrc:props.imgSrc
+        }
+      
+  return (
+    <figure>
+      {pokemon.imgSrc != null ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
 
 export default PokemonCard;
