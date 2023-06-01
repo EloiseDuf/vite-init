282a40dfd4f569f5f32f63ec7bc9bca1146e1c16
ELOISE DUFAU <eloise.dufau@gmail.com>
2023-06-01 14:54:11 +0200

    exo 7bis alerts

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 import NavBar from './components/NavBar'
+import { useEffect } from 'react'
 
 const pokemonList = [
   {
@@ -32,14 +33,39 @@ const pokemonList = [
   ];
 
 function App() {
+
   const [pokemonIndex, setPokemonIndex] = useState(0)
-  
+
+  const previousClick = () => {
+    setPokemonIndex(pokemonIndex-1)
+  }
+
+  const nextClick= () => {
+    setPokemonIndex(pokemonIndex+1)
+  }
+
+  useEffect(
+    () => {
+    alert(`hello pokemon trainer :)`)
+},
+[]
+  );
+
+  useEffect(
+    () => {
+    pokemonIndex==3 ? alert('pika!pika!pikachu!!!!') : <p></p>
+},
+[pokemonIndex]
+  );
+
   return (
     <>
       <PokemonCard name={pokemonList[pokemonIndex].name} imgSrc={pokemonList[pokemonIndex].imgSrc}  />
-      <NavBar pokemonIndex={pokemonIndex} setPokemonIndex={setPokemonIndex} pokemonList={pokemonList}/>
+      <NavBar previousClick={previousClick} nextClick={nextClick} pokemonIndex={pokemonIndex} pokemonList={pokemonList}/>
     </>
   )
+  
 }
 
-export default App
+
+export default  App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,14 @@
 import App from "../App"
 
-const NavBar =({pokemonIndex,setPokemonIndex,pokemonList}) => {
-const previousClick = () => {
-    setPokemonIndex(pokemonIndex-1)
-  }
-
-  const nextClick= () => {
-    setPokemonIndex(pokemonIndex+1)
-  }
+const NavBar =({pokemonIndex,previousClick,nextClick,pokemonList}) => {
+    
 return (
     <>
 {pokemonIndex>0 ? <button onClick={previousClick}>Previous</button> : <p></p> }
 {pokemonIndex<pokemonList.length-1? <button onClick={nextClick}>Next</button> : <p></p>}
     </>
 )
+
 }
 
 export default NavBar;
